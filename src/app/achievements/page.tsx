0fc657623181b00f9051e5981cb95fbc52c26445
Shakeef Ahmed Rakin <shakeef.rakin321@gmail.com>
2024-11-18 18:11:35 +0600

    Prepared metadata for achievments page and added last award details.

--- src/app/achievements/page.tsx
+++ src/app/achievements/page.tsx
@@ -2,6 +2,57 @@ import Image from "next/image";
 import awards from "../_data/awards.json";
 import Link from "next/link";
 import { IoIosArrowForward } from "react-icons/io";
+import { Metadata } from "next";
+
+export function generateMetadata(): Metadata {
+  return {
+    // GOOGLE INDEXING SEO TAGS
+    title: "Shakeef Ahmed Rakin - Achievements | Hackathons | Competitions",
+    description:
+      "Explore the achievements of Shakeef Ahmed Rakin, showcasing various competition experiences in full-stack development, machine learning, and more.",
+    keywords: [
+      "Shakeef Ahmed Rakin",
+      "Achievements",
+      "Portfolio",
+      "Competitions",
+      "Machine Learning",
+      "Full-Stack Development",
+      "Hackathons",
+    ],
+    publisher: "Shakeef Ahmed Rakin",
+    alternates: {
+      canonical: `${process.env.WEBSITE_URL}/achievements`,
+    },
+    robots: "index, follow",
+
+    // PREVIEW TAGS FOR SOCIAL MEDIA
+    openGraph: {
+      title: "Achievements | Shakeef Ahmed Rakin",
+      type: "website",
+      description:
+        "Explore the achievements of Shakeef Ahmed Rakin, showcasing various competition experiences in full-stack development, machine learning, and more.",
+      url: `${process.env.WEBSITE_URL}/achievements`,
+      images: [
+        {
+          url: "/og_images/achievementspage.png",
+          alt: "Shakeef Ahmed Rakin - Achievements",
+        },
+      ],
+      siteName: "Shakeef Ahmed Rakin - Portfolio",
+    },
+    twitter: {
+      title: "Achievements | Shakeef Ahmed Rakin",
+      description:
+        "Showcasing the achievements of Shakeef Ahmed Rakin across fields like full-stack development, machine learning, and hackathons.",
+      images: [
+        {
+          url: "/og_images/achievementspage.png",
+          alt: "Shakeef Ahmed Rakin - Achievements",
+        },
+      ],
+    },
+  };
+}
 
 export default function Achievements() {
   return (
