293b3033a218bac0762a378d447c2cd5dec64306
Shakeef Ahmed Rakin <shakeef.rakin321@gmail.com>
2024-11-18 19:08:48 +0600

    Fixed redirect issue.

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,15 @@ const nextConfig: NextConfig = {
       "i.postimg.cc",
     ], // Add the external image domain here
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*", // This matches any path
+        destination: "https://shakeefahmedrakin.vercel.app/:path*", // Redirects to non-www version
+        permanent: true, // This is a 301 permanent redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
