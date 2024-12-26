74ed300690f643031c81ea6ed83069d817801b0f
Shakeef Ahmed Rakin <shakeef.rakin321@gmail.com>
2024-12-26 08:42:47 +0600

    testing schema.

---
+++ src/app/_util/metadata/AboutPage/SetSchemaAboutPage.tsx
@@ -0,0 +1,54 @@
+import { WebPage, WithContext } from "schema-dts";
+import work from "../../../_data/work.json";
+import education from "../../../_data/education.json";
+import { skills } from "@/app/_data/skills";
+
+export function SetSchemaAboutPage() {
+  const jsonLd: WithContext<WebPage> = {
+    "@context": "https://schema.org",
+    "@type": "WebPage",
+    name: "About Shakeef Ahmed Rakin",
+    description:
+      "Learn more about Shakeef Ahmed Rakin, a Full Stack Engineer from Bangladesh with expertise in web development, problem-solving, and machine learning.",
+    url: `${process.env.WEBSITE_URL}/about`,
+    mainEntity: {
+      "@type": "Person",
+      name: "Shakeef Ahmed Rakin",
+      additionalName: "Rakin",
+      description: `Born and raised in Dhaka, Bangladesh, I started my coding journey during my undergraduate studies in Computer Science and Engineering. Towards the end of my university life, I actively participated in courses and competitions, where I gained hands-on experience in a wide range of fields, from full-stack web development to machine learning. These experiences helped me develop a strong foundation in both problem-solving and technical skills. Beyond work, I enjoy music, gaming, and anime.`,
+      birthDate: "2002-11-07",
+      birthPlace: "Dhaka, Bangladesh",
+      email: "mailto:shakeef.rakin321@gmail.com",
+      gender: "Male",
+      nationality: "Bangladeshi",
+      jobTitle: work.map((item) => item.position).join(", "),
+      worksFor: work.map((item) => ({
+        "@type": "Organization",
+        name: item.company,
+        url: item.companyLink ?? undefined,
+        logo: `${process.env.WEBSITE_URL}${item.companyLogo}`,
+      })),
+      alumniOf: education.map((item) => ({
+        "@type": "EducationalOrganization",
+        name: item.institution,
+        url: item.url,
+      })),
+      knowsAbout: skills.flatMap((item) =>
+        item.skills.map((skill) => skill.name)
+      ),
+      knowsLanguage: ["English", "Bengali"],
+      image: `${process.env.WEBSITE_URL}/og_images/aboutpage.png`,
+      sameAs: [
+        "https://www.linkedin.com/in/shakeef-ahmed-rakin/",
+        "https://github.com/ShakeefAhmedRakin",
+      ],
+    },
+  };
+
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+    />
+  );
+}
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,78 +1,13 @@
-import { Person, WithContext } from "schema-dts";
-import work from "./../_data/work.json";
 import LayoutWrapper from "../_components/wrappers/LayoutWrapper";
 import TitleLarge from "../_components/ui/TitleLarge";
 import AboutMeSection from "./_components/AboutMeSection";
 import SkillsSection from "./_components/SkillsSection";
+import { SetSchemaAboutPage } from "../_util/metadata/AboutPage/SetSchemaAboutPage";
 
-export default function About() {
-  // Prepare JSON-LD
-  const jsonLd: WithContext<Person> = {
-    "@context": "https://schema.org",
-    "@type": "Person",
-    name: "Shakeef Ahmed Rakin",
-    additionalName: "Rakin",
-    affiliation: work.map((item) => {
-      return { "@type": "Organization", name: item.company };
-    }),
-    alumniOf: [
-      {
-        "@type": "EducationalOrganization",
-        name: "Cambridge International School",
-        url: "https://www.cambridgeinternational.org",
-      },
-      {
-        "@type": "EducationalOrganization",
-        name: "BRAC University",
-        url: "https://www.bracu.ac.bd",
-      },
-    ],
-    birthDate: "2002-11-07",
-    birthPlace: "Dhaka, Bangladesh",
-    email: "mailto:shakeef.rakin321@gmail.com",
-    gender: "Male",
-    hasOccupation: {
-      "@type": "Occupation",
-      name: "Software Engineer",
-      skills: [
-        "Full-Stack Development",
-        "Machine Learning",
-        ".NET Development",
-        "UI/UX Design",
-      ],
-    },
-    jobTitle: [
-      "Full Stack Developer",
-      "Full Stack Engineer",
-      ".NET Software Developer",
-      "Machine Learning Engineer",
-    ],
-    nationality: "Bangladeshi",
-    worksFor: work.map((item) => {
-      return { "@type": "Organization", name: item.company };
-    }),
-    image: "/hero.jpg",
-    url: process.env.WEBSITE_URL,
-    sameAs: [
-      "https://www.linkedin.com/in/shakeef-ahmed-rakin/",
-      "https://github.com/ShakeefAhmedRakin",
-    ],
-    knowsAbout: [
-      "Web Development",
-      "Machine Learning",
-      "MERN Stack",
-      ".NET",
-      "Software Engineering",
-    ],
-    knowsLanguage: ["English", "Bengali"],
-  };
-
+export default function AboutPage() {
   return (
     <>
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-      />
+      <SetSchemaAboutPage />
       <LayoutWrapper className="bg-backgroundDark">
         <TitleLarge firstTitle="About" secondTitle="Me" isPrimary={true} />
         <AboutMeSection />
