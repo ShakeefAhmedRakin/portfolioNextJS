3a888d13fa4ace1472941812884de9e99477e351
Shakeef Ahmed Rakin <shakeef.rakin321@gmail.com>
2025-02-05 10:50:08 +0600

    Fixed build error

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -14,7 +14,7 @@ export default function NotFoundPage() {
       <div className="px-4 flex flex-col gap-5 text-center items-center justify-center bg-background min-h-[calc(100vh-20rem)] rounded-lg">
         <h1 className="text-primary font-body text-6xl font-bold">404</h1>
         <h2 className="text-text font-body text-3xl font-semibold">
-          Page Doesn't Exist
+          {`Page Doesn't Exist`}
         </h2>
         <p className="text-text font-body font-extralight">
           {`Oops! The page you're looking for doesn't seem to exist`}
